2f0c6fe3453380e9c9e17acc751ccebdaca6a185
leejeongmin <danmin20@khu.ac.kr>
2020-04-01 23:52:38 +0900

    components

---
+++ src/components/item-input.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="form-group">
+    <input type="text" class="form-control" placeholder="할일을 입력해주세요." @keyup.enter="additem">
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component
+export default class ItemInput extends Vue{
+    additem(){
+        return 0;
+    }
+}
+</script>
---
+++ src/components/item.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" aria-label="..." @change="changeStatus">
+      </span>
+      <input type="text" class="form-control" aria-label="...">
+        <span class="input-group-btn">
+        <button class="btn btn-default" type="button" @click="removeItem">Go!</button>
+      </span>
+    </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component
+export default class Item extends Vue{
+    changeStatus(){
+        return 0;
+    }
+    removeItem(){
+        return 0;
+    }
+}
+</script>
